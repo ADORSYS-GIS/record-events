f64af2924f21a2eeb6e1f399ef829404bbf168f3
Francis Pouatcha <francis.pouatcha@adorsys.com>
2025-10-10 03:02:12 +0100

    Fix: eslint errors
    Signed-off-by: Francis Pouatcha <francis.pouatcha@adorsys.com>

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,25 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "dev-dist", "/api/generated/**"] },
+  {
+    ignores: [
+      "dist",
+      "dev-dist",
+      "/api/generated/**",
+      "src/labels/cameroon-data.ts",
+      "src/labels/cameroon-data20-1.ts",
+      "src/labels/cameroon-data21-1.ts",
+      "src/labels/cameroon-data22-1.ts",
+      "src/labels/cameroon-data23-1.ts",
+      "src/labels/cameroon-data24-1.ts",
+      "src/labels/cameroon-data25-1.ts",
+      "src/labels/cameroon-data26-1.ts",
+      "src/labels/cameroon-data27-1.ts",
+      "src/labels/cameroon-data28-1.ts",
+      "src/labels/cameroon-data29-1.ts",
+      "src/labels/cameroon-data30-1.ts",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
